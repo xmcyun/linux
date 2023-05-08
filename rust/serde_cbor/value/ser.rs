f5c14f204cdbe300342f770380394c56aed535c3
Ariel Miculas <amiculas@cisco.com>
2023-05-08 19:36:32 +0300

    rust: serde_cbor: add SPDX License Identifiers

--- rust/serde_cbor/value/ser.rs
+++ rust/serde_cbor/value/ser.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 // Copyright 2017 Serde Developers
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
--- rust/serde_cbor/write.rs
+++ rust/serde_cbor/write.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 #[cfg(feature = "alloc")]
 use alloc::vec::Vec;
 #[cfg(not(feature = "std"))]
