72a741d7a63b3a883ad7ebd55aa15f8c8f5e49d3
Ariel Miculas <amiculas@cisco.com>
2023-05-31 12:59:31 +0300

    rust: puzzlefs: add HexError to WireFormatError and implement the From conversion
    Signed-off-by: Ariel Miculas <amiculas@cisco.com>

--- samples/rust/puzzle/error.rs
+++ samples/rust/puzzle/error.rs
@@ -12,6 +12,7 @@ pub(crate) enum WireFormatError {
     CBORError(serde_cbor::Error),
     KernelError(kernel::error::Error),
     TryReserveError(TryReserveError),
+    HexError(hex::FromHexError),
 }
 
 impl WireFormatError {
@@ -23,6 +24,7 @@ impl WireFormatError {
             WireFormatError::CBORError(..) => kernel::error::Error::to_errno(EINVAL),
             WireFormatError::KernelError(e) => kernel::error::Error::to_errno(*e),
             WireFormatError::TryReserveError(_) => kernel::error::Error::to_errno(EINVAL),
+            WireFormatError::HexError(_) => kernel::error::Error::to_errno(EINVAL),
         }
     }
 
@@ -40,6 +42,7 @@ impl Display for WireFormatError {
             WireFormatError::CBORError(_) => f.write_str("CBOR error"),
             WireFormatError::KernelError(_) => f.write_str("Kernel error"),
             WireFormatError::TryReserveError(_) => f.write_str("TryReserveError"),
+            WireFormatError::HexError(_) => f.write_str("HexError"),
         }
     }
 }
@@ -68,5 +71,13 @@ impl core::convert::From<TryReserveError> for WireFormatError {
     #[allow(deprecated)]
     fn from(source: TryReserveError) -> Self {
         WireFormatError::TryReserveError(source)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::convert::From<hex::FromHexError> for WireFormatError {
+    #[allow(deprecated)]
+    fn from(source: hex::FromHexError) -> Self {
+        WireFormatError::HexError(source)
     }
 }
